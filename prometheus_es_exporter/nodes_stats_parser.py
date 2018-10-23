42cdf1a3747e73e187569fab4d661ca09a3572be
[Redacted] <oneohtrix@users.noreply.github.com>
2018-10-23 18:16:56 +0200

    Fix shard parsing

--- prometheus_es_exporter/nodes_stats_parser.py
+++ prometheus_es_exporter/nodes_stats_parser.py
@@ -47,13 +47,15 @@ def parse_block(block, metric=[], labels={}):
                         singular_key = key
                     for n_key, n_value in value.items():
                         if isinstance(n_value, list):
-                            # n_value get passed back into parse_block,
-                            # however parse_block expects a dict, not list
-                            n_value = dict(("shard_id", d) for d in n_value)
-                        if key in excluded_metric_keys:
-                            result.extend(parse_block(n_value, metric=metric, labels=merge_dicts(labels, {singular_key: [n_key]})))
+                            if key == "shards":
+                                n_list = [{"shard_id": d} for d in n_value]
+                                for n_value in n_list:
+                                    result.extend(parse_block(n_value, metric=metric + [key], labels=merge_dicts(labels, {singular_key: [n_key]})))
                         else:
-                            result.extend(parse_block(n_value, metric=metric + [key], labels=merge_dicts(labels, {singular_key: [n_key]})))
+                            if key in excluded_metric_keys:
+                                result.extend(parse_block(n_value, metric=metric, labels=merge_dicts(labels, {singular_key: [n_key]})))
+                            else:
+                                result.extend(parse_block(n_value, metric=metric + [key], labels=merge_dicts(labels, {singular_key: [n_key]})))
                 else:
                     result.extend(parse_block(value, metric=metric + [key], labels=labels))
             elif isinstance(value, list) and key in bucket_list_keys:
